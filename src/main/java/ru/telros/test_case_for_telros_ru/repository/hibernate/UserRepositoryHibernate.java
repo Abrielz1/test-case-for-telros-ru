a67cd6ed9d72de4ae57b4a2f7e14f938e1b920f4
Abriel <106871954+Abrielz1@users.noreply.github.com>
2024-10-14 01:08:12 +0300

    Delete src/main/java/ru/telros/test_case_for_telros_ru/repository/hibernate directory
    hibernate is off for now

--- src/main/java/ru/telros/test_case_for_telros_ru/repository/hibernate/UserRepositoryHibernate.java
+++
@@ -1,4 +0,0 @@
-package ru.telros.test_case_for_telros_ru.repository.hibernate;
-
-public interface UserRepositoryHibernate {
-}
--- src/main/java/ru/telros/test_case_for_telros_ru/repository/hibernate/UserRepositoryHibernateImpl.java
+++
@@ -1,8 +0,0 @@
-package ru.telros.test_case_for_telros_ru.repository.hibernate;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UserRepositoryHibernateImpl implements UserRepositoryHibernate {
-
-}
